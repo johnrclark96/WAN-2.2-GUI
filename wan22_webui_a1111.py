b597ff1320564308b753a85eeb2e21d2e4b7f980
John Clark <johnrclark96@gmail.com>
2025-08-21 14:51:57 -0500

    Console progress bar and patching

--- wan22_webui_a1111.py
+++ wan22_webui_a1111.py
@@ -1,1 +1,4 @@
-﻿import sys try:     sys.stdout.write(line)     sys.stdout.flush() except Exception:     pass
+﻿    import sys     try:         sys.stdout.write(line)         sys.stdout.flush()     except Exception:         pass
+import sys try:     sys.stdout.write(line)     sys.stdout.flush() except Exception:     pass
+s
+i
